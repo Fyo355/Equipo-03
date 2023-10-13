37e05ba914517207ef1c77e9008bbffdc2ed2b31
juanandma <juanandma@disroot.org>
2023-10-13 18:42:01 +0200

    Eliminamos comentario

--- src/infrastructure/API/Controllers/PostUserController.test.js
+++ src/infrastructure/API/Controllers/PostUserController.test.js
@@ -53,31 +53,4 @@ describe("PostUserController", () => {
 
     expect(res.status).toHaveBeenCalledWith(201)
   })
-
-  /*
-  it("allows the new user to login", async () => {
-    const registerUser = { execute: vi.fn() }
-    const postUserController = new PostUserController(registerUser)
-    const name = "John Doe"
-    const email = "johndoe@example.com"
-    const password = "password"
-    const age = 18
-    const req = {
-      body: {
-        name,
-        email,
-        password,
-        age,
-      },
-    }
-    json = vi.fn()
-    res = {
-      status: vi.fn(() => ({ json })),
-    }
-
-    await postUserController.execute(req, res)
-
-    expect(res.status).toHaveBeenCalledWith(201)
-  })
-  */
 })
